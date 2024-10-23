68077eab3c3303af26e89c76c561c491f3fa641b
Cramunhao23 <souzaluizfelipe37@outlook.com>
2024-10-23 12:30:19 -0300

    first commit

---
+++ app/src/main/java/com/example/ancora/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.ancora
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.ancora.ui.theme.AncoraTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        installSplashScreen()
+        setContent {
+            AncoraTheme {
+                Scaffold { innerPadding ->
+                    Greeting(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AncoraTheme {
+        Greeting()
+    }
+}
