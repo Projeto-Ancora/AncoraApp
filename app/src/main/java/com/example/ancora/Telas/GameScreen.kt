e8b30d478f8dd7178fad2e40b2ad6035e2528b39
LFTSouza <souzaluizfelipe37@outlook.com>
2024-10-28 15:43:13 -0300

    tela de games update 2

--- app/src/main/java/com/example/ancora/Telas/GameScreen.kt
+++ app/src/main/java/com/example/ancora/Telas/GameScreen.kt
@@ -1,8 +1,8 @@
 package com.example.ancora.Telas
 
 import android.util.Log
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,22 +10,21 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.ClickableText
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.ancora.onClick
+import androidx.compose.ui.unit.sp
+import com.example.ancora.R
+
 
 @Composable
 fun GameScree(modifier: Modifier = Modifier) {
@@ -54,9 +53,7 @@ fun GameScree(modifier: Modifier = Modifier) {
 
         ClickableText(
             text = AnnotatedString("2X2"),
-            onClick = { boolCLick ->
-                Log.d("ClickableText", "$boolCLick -th character is clicked.")
-            },
+            onClick = { getMult(2 * 2) },
             modifier = modifier
                 .drawBehind {
                     drawCircle(
@@ -70,9 +67,7 @@ fun GameScree(modifier: Modifier = Modifier) {
 
         ClickableText(
             text = AnnotatedString("2X2"),
-            onClick = { boolCLick ->
-                Log.d("ClickableText", "$boolCLick -th character is clicked.")
-            },
+            onClick = {},
             modifier = modifier
                 .drawBehind {
                     drawCircle(
@@ -82,8 +77,69 @@ fun GameScree(modifier: Modifier = Modifier) {
                 }
         )
     }
+
+    Column(
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+            .fillMaxSize()
+            .padding(start = 250.dp)
+    ) {
+        Box {
+            Image(
+                painter = painterResource(R.drawable.peixe),
+                contentDescription = "Peixe",
+                modifier
+                    .size(130.dp)
+            )
+            Text(
+                text = "4",
+                modifier
+                    .padding(top = 56.dp)
+                    .padding(start = 69.dp)
+                    .clickable { compararResultado() },
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
+            )
+        }
+
+        Image(
+            painter = painterResource(R.drawable.peixe),
+            contentDescription = "Peixe",
+            modifier
+                .size(130.dp)
+        )
+
+        Image(
+            painter = painterResource(R.drawable.peixe),
+            contentDescription = "Peixe",
+            modifier
+                .size(130.dp)
+        )
+    }
 }
 
+val caseMult = mutableListOf<Int>();
+
+private fun getMult(num: Int) {
+    caseMult.add(num)
+}
+
+private fun setMult(): List<Int> {
+    return caseMult.toList();
+}
+
+private fun compararResultado() {
+    when {
+        setMult().contains(4) -> {
+            Log.d("Acertou", "Acertou")
+        }
+
+        else -> {
+            Log.d("Errou", "Errou")
+        }
+    }
+}
 
 @Preview
 @Composable
